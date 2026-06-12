19904ee2fba843cabecc3c725b1e91c7512eafa6
DavidLeeftink <39411160+DavidLeeftink@users.noreply.github.com>
2026-06-12 09:45:52 +0000

    Deploying to gh-pages from @ DavidLeeftink/davidleeftink.github.io@66bc883501750232aa3226dfdbae6deda21d2160 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,8 +84,8 @@ ninja.data = [{
           section: "News",},{id: "news-paper-accepted-our-recent-work-on-bayesian-optimization-for-semiconductor-manufacturing-has-been-accepted-for-ifac-s-control-engineering-practice",
           title: 'Paper accepted: Our recent work on Bayesian Optimization for Semiconductor Manufacturing has been...',
           description: "",
-          section: "News",},{id: "news-winner-poster-prize-donders-day-very-happy-to-have-received-the-poster-award-for-the-natural-computing-and-neurotechnology-theme-at-the-donders-day-of-2026-i-presented-a-poster-on-our-recent-preprint-on-neural-co-state-policies",
-          title: 'Winner Poster Prize Donders Day: Very happy to have received the poster award...',
+          section: "News",},{id: "news-winner-poster-prize-very-happy-to-have-received-the-poster-award-for-the-natural-computing-and-neurotechnology-theme-at-the-donders-day-of-2026-i-presented-on-our-recent-preprint-on-neural-co-state-policies-check-out-the-photo-here",
+          title: 'Winner Poster Prize: Very happy to have received the poster award for the...',
           description: "",
           section: "News",},{
         id: 'social-bluesky',
